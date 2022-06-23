bc343867d5f83c3e1cb219c2af4d6c9909a9d81b
Sami Abdul-Wahid <syntapy@pm.me>
2022-06-22 20:18:31 -0700

    uses typescript

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+	<React.StrictMode>
+		<h1>Hello Nuclear Reactors</h1>
+	</React.StrictMode>
+)
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,28 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-	entry: './src/main.jsx',
+	entry: './src/main.tsx',
 	mode: 'development',
 	module: {
 		rules: [
 			{
 				test: /\.(js|jsx)$/,
 				use: ['babel-loader'],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+				exclude: /node_modules/
+			},
+			{
+				test: /\.sass$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
 				exclude: /node_modules/
 			}
 		]
