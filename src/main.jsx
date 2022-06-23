83d0d22348f500c6239d4484e1eca904d4662b5c
Sami Abdul-Wahid <syntapy@pm.me>
2022-06-22 19:40:50 -0700

    works, with javascript

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const title = 'Reactive boilerplate'
-
-root = createRoot(document.getElementById('root'))
-root.render(<h1>{title}</h1>)
+root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+	<React.StrictMode>
+		<h1>Hello Nuclear Reactors</h1>
+	</React.StrictMode>
+)
